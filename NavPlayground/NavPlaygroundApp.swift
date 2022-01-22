c57e7673140f2a3e68dcba71b422a9a7845746e3
Zev Eisenberg <zev.eisenberg@nytimes.com>
2022-01-22 14:33:48 -0500

    Initial commit.

---
+++ NavPlayground/NavPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavPlaygroundApp.swift
+//  NavPlayground
+//
+//  Created by Zev Eisenberg on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct NavPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
